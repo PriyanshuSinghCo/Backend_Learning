d641039f55e7b54265138e805a8e8551b56b1b6b
Priyanshu Singh <priyanshu2002krsingh@gmail.com>
2026-02-01 01:22:56 +0530

    pbkdf2 done

--- Key_Derivation_function/app.js
+++ Key_Derivation_function/app.js
@@ -1,9 +1,11 @@
 import  crypto from "node:crypto";
 
+const salt = crypto.randomBytes(16);
 
+crypto.pbkdf2("password", salt, 100000,32, "sha256", (error, output) => {
+    console.log(output.toString("base64url"));
+    console.log(salt.toString("base64url"));
+});
 
-// crypto.pbkdf2("password", "salt_sudh", 100000,32, "sha256", (error, output) => {
-//     console.log(output.toString("base64url"));
-// });
 
 
