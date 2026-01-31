8e563ca24316d45c804d6b8b833188c94d0b2511
Priyanshu Singh <priyanshu2002krsingh@gmail.com>
2026-02-01 00:18:48 +0530

    key_Derivation_Funtion

---
+++ Key_Derivation_function/app.js
@@ -0,0 +1,9 @@
+import  crypto from "node:crypto";
+
+
+
+// crypto.pbkdf2("password", "salt_sudh", 100000,32, "sha256", (error, output) => {
+//     console.log(output.toString("base64url"));
+// });
+
+
