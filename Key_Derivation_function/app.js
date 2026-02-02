52c5e8649c6908cab2fe4b21698ed10855dac915
Priyanshu Singh <priyanshu2002krsingh@gmail.com>
2026-02-02 23:55:57 +0530

    bcrypt

--- Key_Derivation_function/app.js
+++ Key_Derivation_function/app.js
@@ -1,11 +1,17 @@
-import  crypto from "node:crypto";
+// import  crypto from "node:crypto";
 
-const salt = crypto.randomBytes(16);
+// const salt = crypto.randomBytes(16);
 
-crypto.pbkdf2("password", salt, 100000,32, "sha256", (error, output) => {
-    console.log(output.toString("base64url"));
-    console.log(salt.toString("base64url"));
-});
+// crypto.pbkdf2("password", salt, 100000,32, "sha256", (error, output) => {
+//     console.log(output.toString("base64url"));
+//     console.log(salt.toString("base64url"));
+// });
+
+
+// bcrypt
+import bcrypt from "bcrypt";
+// const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash("password", 15);
 
 
 
